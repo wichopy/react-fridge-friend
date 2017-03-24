a281ea4352419c9f161e88b72529a62438fe1e7e
william chou <w.chou06@gmail.com>
2017-03-24 16:13:23 -0400

    working example

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GroceryList from './GroceryList.jsx';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default class App extends Component {
   constructor(props){
@@ -12,15 +12,20 @@ export default class App extends Component {
       fridge: ['banana', 'cucumber', 'chicken']
     }
   }
+  newMessageFromServer(event) {
+    console.log(event)
+    this.setState({groceries: this.state.groceries.concat(event.data)});
+  }
   componentDidMount() {
-    const ws = new WebSocket('ws://localhost:5000');
+    const ws = new WebSocket('ws://localhost:3001');
     this.socket = ws;
     this.socket.onopen = () => {
       console.log('Connected to server');
     }
     this.socket.onmessage = (event) => {
       // console.log(event.data);
-      this.newMessageFromServer(event.data);
+      console.log("received data from database");
+      this.newMessageFromServer(event);
     }
     this.socket.onclose = () => {
       console.log('closing socket!');
@@ -33,20 +38,19 @@ mutable: Array#push, Array#splice, for loops w/ direct access
  */
   newFoodItem (food) {
     this.socket.send(JSON.stringify(food));
-    this.setState({groceries: this.state.groceries.concat(food)});
   }
 
   findShelfLife(food){
-    return axios({
-      method: 'get',
-      url: 'https://shelf-life-api.herokuapp.com/search?q='+food,
-         withCredentials: false,
-        // headers: {'X-Requested-With': "XMLHttpRequest"}
-    })
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+    // return axios({
+    //   method: 'get',
+    //   url: 'https://shelf-life-api.herokuapp.com/search?q='+food,
+    //      withCredentials: false,
+    //     // headers: {'X-Requested-With': "XMLHttpRequest"}
+    // })
+    // .then((data) => {
+    //   console.log(data);
+    //   return data;
+    // });
   }
 
   render() {
